46cc8285153e7d67280b3ca723cb8f2c3cb34ee0
lucie <manarinlucie@gmail.com>
2025-04-01 12:19:12 +0200

    fix: variable d'environnement

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'DATABASE_NAME', // Database name
-  'DB_USER', // Users
-  'DB_PASSWORD', // Password
+  'process.env.DB_NAME', // Database name
+  'process.env.DB_USER', // User
+  'process.env.DB_PASSWORD', // Password
   {
-    host: 'DB_HOST', // Host
+    host: 'process.env.DB_HOST', // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
