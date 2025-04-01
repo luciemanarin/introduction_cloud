28a44f9d87a139c59fc69526836717413efda107
lucie <manarinlucie@gmail.com>
2025-04-01 12:15:21 +0200

    add: decommenté

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
-  '', // Password
+  'DATABASE_NAME', // Database name
+  'DB_USER', // Users
+  'DB_PASSWORD', // Password
   {
-    host: '', // Host
+    host: 'DB_HOST', // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
